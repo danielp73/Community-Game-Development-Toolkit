356849ed670214bf1e3f4ca8d37c29f74bd4dc2d
danielp73 <60300214+danielp73@users.noreply.github.com>
2022-05-07 18:34:22 -0400

    clean up unneeded files

--- Assets/CGDT Components/CGDT Control/TextureImport.cs
+++ Assets/CGDT Components/CGDT Control/TextureImport.cs
@@ -1,54 +1,54 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System.Collections;
+﻿//using UnityEditor;
+//using UnityEngine;
+//using System.Collections;
 
-public class TextureImport : AssetPostprocessor
-{
-    void OnPreprocessTexture()
-    {
-        Debug.Log("Imported texture");
+//public class TextureImport : AssetPostprocessor
+//{
+//    void OnPreprocessTexture()
+//    {
+//        Debug.Log("Imported texture");
 
-        TextureImporter textureImporter = (TextureImporter)assetImporter;
-        //textureImporter.maxTextureSize = 512;
-        //textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
+//        TextureImporter textureImporter = (TextureImporter)assetImporter;
+//        //textureImporter.maxTextureSize = 512;
+//        //textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
 
-        int startIndex = assetPath.LastIndexOf("/") + 1;
-        string assetName = assetPath.Substring(startIndex, assetPath.Length - startIndex);
-        if (assetName.EndsWith(".png") || assetName.EndsWith(".PNG"))
-        {
-            Debug.Log("CGDT: Imported PNG");
-            //set texture type to sprite
-            textureImporter.textureType = TextureImporterType.Sprite;
-        }
-        if (assetName.EndsWith(".jpg") || assetName.EndsWith(".JPG"))
-        {
-            Debug.Log("CGDT: Imported JPG");
-            //set texture type to sprite
-            textureImporter.textureType = TextureImporterType.Sprite;
-        }
-        if (assetName.EndsWith(".jpeg") || assetName.EndsWith(".JPEG"))
-        {
-            Debug.Log("CGDT: Imported JPEG");
-            //set texture type to sprite
-            textureImporter.textureType = TextureImporterType.Sprite;
-        }
+//        int startIndex = assetPath.LastIndexOf("/") + 1;
+//        string assetName = assetPath.Substring(startIndex, assetPath.Length - startIndex);
+//        if (assetName.EndsWith(".png") || assetName.EndsWith(".PNG"))
+//        {
+//            Debug.Log("CGDT: Imported PNG");
+//            //set texture type to sprite
+//            textureImporter.textureType = TextureImporterType.Sprite;
+//        }
+//        if (assetName.EndsWith(".jpg") || assetName.EndsWith(".JPG"))
+//        {
+//            Debug.Log("CGDT: Imported JPG");
+//            //set texture type to sprite
+//            textureImporter.textureType = TextureImporterType.Sprite;
+//        }
+//        if (assetName.EndsWith(".jpeg") || assetName.EndsWith(".JPEG"))
+//        {
+//            Debug.Log("CGDT: Imported JPEG");
+//            //set texture type to sprite
+//            textureImporter.textureType = TextureImporterType.Sprite;
+//        }
 
-        //    {
-        //        textureImporter.textureType = TextureImporterType.Sprite;
-        //    }
+//        //    {
+//        //        textureImporter.textureType = TextureImporterType.Sprite;
+//        //    }
 
 
-        //    if (assetName.StartsWith("s_"))
-        //    {
-        //        textureImporter.textureType = TextureImporterType.Sprite;
-        //    }
-        //    else if (assetName.StartsWith("t_"))
-        //    {
-        //        textureImporter.textureType = TextureImporterType.Default;
-        //    }
-        //    else if (assetName.StartsWith("n_"))
-        //    {
-        //        textureImporter.convertToNormalmap = true;
-        //    }
-    }
-}
+//        //    if (assetName.StartsWith("s_"))
+//        //    {
+//        //        textureImporter.textureType = TextureImporterType.Sprite;
+//        //    }
+//        //    else if (assetName.StartsWith("t_"))
+//        //    {
+//        //        textureImporter.textureType = TextureImporterType.Default;
+//        //    }
+//        //    else if (assetName.StartsWith("n_"))
+//        //    {
+//        //        textureImporter.convertToNormalmap = true;
+//        //    }
+//    }
+//}
