a6638a8f165a1b9983378798cf326b9cffcecd5c
aroth638 <ameliaroth11@gmail.com>
2022-06-22 12:56:33 -0400

    Adding SimpleObjectMove to my branch of the editor

---
+++ CGDT Components/ObjectSelection/SimpleObjectMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update to run only if I am selected
+
+        //combine with your Click script
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            Vector3 updatedPosition = transform.position;
+            updatedPosition.x -= 1;
+            transform.position = updatedPosition;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Vector3 updatedPosition = transform.position;
+            updatedPosition.x += 1;
+            transform.position = updatedPosition;
+        }
+    }
+}
+
+
