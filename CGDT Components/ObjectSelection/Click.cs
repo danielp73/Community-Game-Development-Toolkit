c1eb41c073dde883b62e4206dabfe91fa42adee2
aroth638 <ameliaroth11@gmail.com>
2022-06-23 16:32:17 -0400

    Check for syntax errors?

--- CGDT Components/ObjectSelection/Click.cs
+++ CGDT Components/ObjectSelection/Click.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Click : MonoBehaviour
 {
-    private GameObject selected;
-    private GameObject oldSelected;
+    public GameObject selected;
+    public GameObject oldSelected;
     
     private Renderer selectionRenderer;
     private Renderer oldSelectionRenderer;
 
-    private Camera _camera;
+    public Camera _camera;
 
     private Color oldColor = Color.white;
     private Color newColor = Color.red;
 
 
     void Start()
-    {
+    {   //gets the camera attached to the player
         _camera = GameObject.Find("Camera").GetComponent<Camera>();
         
 
@@ -31,33 +31,43 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //cast a ray from the center of the camera's screen
+        Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0); //middle of camera view
+        Ray ray = _camera.ScreenPointToRay(point);
+        RaycastHit hit;
 
-        if (Input.GetMouseButtonDown(0)) //if clicked
-        {
-    
-            Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0); //middle of camera view
-            Ray ray = _camera.ScreenPointToRay(point);
-            RaycastHit hit;
-            
+        if (Physics.Raycast(ray, out hit))
+        { 
 
-            if (Physics.Raycast(ray, out hit))
-            {
+            if (Input.GetMouseButtonDown(0) && !selected) //if clicked
+            {   
                 selected = hit.transform.gameObject;
 
-                if (selected != oldSelected){
-
-                    if (oldSelected != null) //returns previous selection to original color
-                    {
+                if (selected != oldSelected) //if the object wasn't already selected
+                {
+                    if (oldSelected != null) //if there was a previous object
+                    { //returns previous selection to original color
                         oldSelectionRenderer = oldSelected.GetComponent<Renderer>();
                         oldSelectionRenderer.material.color = oldColor;
                     }
-
+                    //change the selected object to red
                     selectionRenderer = selected.GetComponent<Renderer>();
                     selectionRenderer.material.color = newColor; //sets a new color
 
-                    oldSelected = selected;
+                    oldSelected = selected; //set the newly red object as the previously selected
                 }
             }
+
+            else if (Input.GetMouseButton(0) && selected)
+            {
+                public GameObject Player = GetComponent<CharacterController>();
+
+                if (Input.GetKey(KeyCode.J))
+                {
+                    transform.RotateAround(Player.transform.position, Vector3.up, 20 * Time.deltaTime);
+                }
+            }
+
             
         }
     }
--- CGDT Components/ObjectSelection/SimpleObjectMove.cs
+++ CGDT Components/ObjectSelection/SimpleObjectMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SimpleObjectMove : MonoBehaviour
 {
+
+    public Click movableObject;
+    
     // Start is called before the first frame update
     void Start()
     {
