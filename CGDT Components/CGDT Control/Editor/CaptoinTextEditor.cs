d73c4d0dff21ba48ea51a329b55d605fb835aae7
Daniel Lichtman <60300214+danielp73@users.noreply.github.com>
2023-09-26 11:12:31 -0400

    Panel text almost working

---
+++ CGDT Components/CGDT Control/Editor/CaptoinTextEditor.cs
@@ -0,0 +1,28 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//using System;
+//using System.IO;
+//using UnityEditor.TerrainTools;
+
+//[CustomEditor(typeof(CaptionText))]
+//public class CaptionTextEditor : Editor
+//{
+//    SphereCollider sc;
+
+//    private void OnEnable()
+//    {
+        
+
+
+
+//    }
+
+//    public override void OnInspectorGUI()
+//    {
+
+
+
+//    }
+//}
